de077ef22d89b92baae795d2c3e73fae9f079d2b
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-09-13 20:02:02 +0800

    增加debug代码

--- src/main/java/com/hexagram2021/custom_worldgen/mixin/TerrainShaperMixin.java
+++ src/main/java/com/hexagram2021/custom_worldgen/mixin/TerrainShaperMixin.java
@@ -1,5 +1,6 @@
 package com.hexagram2021.custom_worldgen.mixin;
 
+import com.hexagram2021.custom_worldgen.common.CWGLogger;
 import net.minecraft.data.worldgen.TerrainProvider;
 import net.minecraft.util.CubicSpline;
 import net.minecraft.util.ToFloatFunction;
@@ -12,68 +13,84 @@ import static com.hexagram2021.custom_worldgen.common.config.CWGCommonConfig.*;
 public class TerrainShaperMixin {
 	@Redirect(method = "overworldOffset", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FF)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 1))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyMushroomField2DeepOceanPoint(CubicSpline.Builder<C, I> instance, float _1, float _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, MUSHROOM_FIELDS_TO_DEEP_OCEAN_SHAPER.value()));
 		return instance.addPoint(MUSHROOM_FIELDS_TO_DEEP_OCEAN_SHAPER.value(), _2);
 	}
 	@Redirect(method = "overworldOffset", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FF)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 2))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyDeepOcean2OceanPoint(CubicSpline.Builder<C, I> instance, float _1, float _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, DEEP_OCEAN_TO_OCEAN_SHAPER.value()));
 		return instance.addPoint(DEEP_OCEAN_TO_OCEAN_SHAPER.value(), _2);
 	}
 	@Redirect(method = "overworldOffset", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FF)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 3))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyOcean2CoastPoint(CubicSpline.Builder<C, I> instance, float _1, float _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, OCEAN_TO_COAST_SHAPER.value()));
 		return instance.addPoint(OCEAN_TO_COAST_SHAPER.value(), _2);
 	}
 	@Redirect(method = "overworldOffset", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FF)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 4))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyCoastWaterPoint(CubicSpline.Builder<C, I> instance, float _1, float _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, COAST_WATER_SHAPER.value()));
 		return instance.addPoint(COAST_WATER_SHAPER.value(), _2);
 	}
 	@Redirect(method = "overworldOffset", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FLnet/minecraft/util/CubicSpline;)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 0))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyCoastBankPoint(CubicSpline.Builder<C, I> instance, float _1, CubicSpline<C, I> _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, COAST_BANK_SHAPER.value()));
 		return instance.addPoint(COAST_BANK_SHAPER.value(), _2);
 	}
 	@Redirect(method = "overworldOffset", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FLnet/minecraft/util/CubicSpline;)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 1))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyCoastLandPoint(CubicSpline.Builder<C, I> instance, float _1, CubicSpline<C, I> _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, COAST_LAND_SHAPER.value()));
 		return instance.addPoint(COAST_LAND_SHAPER.value(), _2);
 	}
 	@Redirect(method = "overworldOffset", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FLnet/minecraft/util/CubicSpline;)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 2))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyNearInlandPoint(CubicSpline.Builder<C, I> instance, float _1, CubicSpline<C, I> _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, NEAR_INLAND_SHAPER.value()));
 		return instance.addPoint(NEAR_INLAND_SHAPER.value(), _2);
 	}
 	@Redirect(method = "overworldOffset", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FLnet/minecraft/util/CubicSpline;)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 3))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyMidInlandPoint(CubicSpline.Builder<C, I> instance, float _1, CubicSpline<C, I> _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, MID_INLAND_SHAPER.value()));
 		return instance.addPoint(MID_INLAND_SHAPER.value(), _2);
 	}
 
 	@Redirect(method = "overworldFactor", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FF)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 0))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyOcean2CoastErosionPoint(CubicSpline.Builder<C, I> instance, float _1, float _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, OCEAN_TO_COAST_CONTINENTALNESS.value()));
 		return instance.addPoint(OCEAN_TO_COAST_CONTINENTALNESS.value(), _2);
 	}
 	@Redirect(method = "overworldFactor", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FLnet/minecraft/util/CubicSpline;)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 0))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyCoastErosionPoint1(CubicSpline.Builder<C, I> instance, float _1, CubicSpline<C, I> _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, COAST_LAND_SHAPER.value()));
 		return instance.addPoint(COAST_LAND_SHAPER.value(), _2);
 	}
 	@Redirect(method = "overworldFactor", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FLnet/minecraft/util/CubicSpline;)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 1))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyCoastErosionPoint2(CubicSpline.Builder<C, I> instance, float _1, CubicSpline<C, I> _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, NEAR_INLAND_SHAPER.value()));
 		return instance.addPoint(NEAR_INLAND_SHAPER.value(), _2);
 	}
 	@Redirect(method = "overworldFactor", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FLnet/minecraft/util/CubicSpline;)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 2))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyCoast2InlandErosionPoint(CubicSpline.Builder<C, I> instance, float _1, CubicSpline<C, I> _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, NEAR_INLAND_TO_MID_INLAND_CONTINENTALNESS.value()));
 		return instance.addPoint(NEAR_INLAND_TO_MID_INLAND_CONTINENTALNESS.value(), _2);
 	}
 	@Redirect(method = "overworldFactor", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FLnet/minecraft/util/CubicSpline;)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 3))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyNearInlandErosionPoint(CubicSpline.Builder<C, I> instance, float _1, CubicSpline<C, I> _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, INLAND_EROSION_SHAPER.value()));
 		return instance.addPoint(INLAND_EROSION_SHAPER.value(), _2);
 	}
 
 	@Redirect(method = "overworldJaggedness", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FF)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 0))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyOcean2CoastJaggednessPoint(CubicSpline.Builder<C, I> instance, float _1, float _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, COAST_TO_INLAND_CONTINENTALNESS.value()));
 		return instance.addPoint(COAST_TO_INLAND_CONTINENTALNESS.value(), _2);
 	}
 	@Redirect(method = "overworldJaggedness", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FLnet/minecraft/util/CubicSpline;)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 0))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyCoast2InlandJaggednessPoint(CubicSpline.Builder<C, I> instance, float _1, CubicSpline<C, I> _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, NEAR_INLAND_TO_MID_INLAND_CONTINENTALNESS.value()));
 		return instance.addPoint(NEAR_INLAND_TO_MID_INLAND_CONTINENTALNESS.value(), _2);
 	}
 	@Redirect(method = "overworldJaggedness", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSpline$Builder;addPoint(FLnet/minecraft/util/CubicSpline;)Lnet/minecraft/util/CubicSpline$Builder;", ordinal = 1))
 	private static <C, I extends ToFloatFunction<C>> CubicSpline.Builder<C, I> modifyMidInland2FarInlandJaggednessPoint(CubicSpline.Builder<C, I> instance, float _1, CubicSpline<C, I> _2) {
+		CWGLogger.LOGGER.debug("%f -> %f".formatted(_1, PEAKS_EROSION_SHAPER.value()));
 		return instance.addPoint(PEAKS_EROSION_SHAPER.value(), _2);
 	}
 }
