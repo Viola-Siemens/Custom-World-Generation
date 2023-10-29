ad02463971139690fa772e8e8dd21b42ce77face
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-10-29 22:54:20 +0800

    1.20，添加获取坐标噪声值的指令

--- src/main/java/com/hexagram2021/custom_worldgen/CustomWorldGeneration.java
+++ src/main/java/com/hexagram2021/custom_worldgen/CustomWorldGeneration.java
@@ -1,5 +1,11 @@
 package com.hexagram2021.custom_worldgen;
 
+import com.hexagram2021.custom_worldgen.server.CWGCommands;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(CustomWorldGeneration.MODID)
@@ -7,5 +13,13 @@ public class CustomWorldGeneration {
 	public static final String MODID = "custom_worldgen";
 
 	public CustomWorldGeneration() {
+		MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@SubscribeEvent
+	public void registerCommands(RegisterCommandsEvent event) {
+		final CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+		dispatcher.register(CWGCommands.register());
 	}
 }
---
+++ src/main/java/com/hexagram2021/custom_worldgen/server/CWGCommands.java
@@ -0,0 +1,71 @@
+package com.hexagram2021.custom_worldgen.server;
+
+import com.hexagram2021.custom_worldgen.common.utils.CWGLogger;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.QuartPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.biome.Climate;
+
+import javax.annotation.Nullable;
+import java.util.function.Consumer;
+
+public final class CWGCommands {
+	private static final String POSITION = "pos";
+	
+	private CWGCommands() {
+	}
+	
+	public static LiteralArgumentBuilder<CommandSourceStack> register() {
+		return Commands.literal("cwg")
+				.requires(stack -> stack.hasPermission(2))
+				.then(
+						Commands.literal("noise")
+								.then(
+										Commands.argument(POSITION, BlockPosArgument.blockPos())
+												.executes(context -> getWorldgenNoise(
+														context.getSource().getPlayer(),
+														context.getSource().getLevel(),
+														BlockPosArgument.getBlockPos(context, POSITION)
+												))
+								)
+				);
+	}
+	
+	private static int getWorldgenNoise(@Nullable ServerPlayer player, ServerLevel level, BlockPos blockPos) {
+		int x = QuartPos.fromBlock(blockPos.getX());
+		int y = QuartPos.fromBlock(blockPos.getY());
+		int z = QuartPos.fromBlock(blockPos.getZ());
+		Climate.TargetPoint targetPoint = level.getChunkSource().randomState().sampler().sample(x, y, z);
+		float c = Climate.unquantizeCoord(targetPoint.continentalness());
+		float e = Climate.unquantizeCoord(targetPoint.erosion());
+		float t = Climate.unquantizeCoord(targetPoint.temperature());
+		float h = Climate.unquantizeCoord(targetPoint.humidity());
+		float w = Climate.unquantizeCoord(targetPoint.weirdness());
+		
+		Consumer<String> printer;
+		if(player == null) {
+			printer = CWGLogger.LOGGER::info;
+		} else {
+			printer = msg -> player.sendSystemMessage(Component.literal(msg));
+		}
+		printDebugInfoTo(printer, blockPos.getX(), blockPos.getY(), blockPos.getZ(), c, e, t, h, w);
+		
+		return Command.SINGLE_SUCCESS;
+	}
+	
+	private static void printDebugInfoTo(Consumer<String> printer, int x, int y, int z, float c, float e, float t, float h, float w) {
+		printer.accept("The worldgen noise parameters of block position (%d, %d, %d) is:".formatted(x, y, z));
+		printer.accept("\tContinentalness: " + c);
+		printer.accept("\tErosion: " + e);
+		printer.accept("\tTemperature: " + t);
+		printer.accept("\tHumidity: " + h);
+		printer.accept("\tWeirdness: " + w);
+	}
+}
